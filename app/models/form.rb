32feb35c8e5d64df8939151b85a1422d8777e857
hirona96 <95573824+hirona96@users.noreply.github.com>
2022-01-28 13:50:30 +0900

    formモデル、テーブルの作成

---
+++ app/models/form.rb
@@ -0,0 +1,14 @@
+class Form < ApplicationRecord
+  enum seat: [:table, :counter], _prefix: true
+
+  with_options presence: true do
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width characters.'}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width characters.'}
+    validates :number_of_people, numericality: { only_integer: true, message: 'is invalid. Input only number'}
+    validates :datetime
+  end
+
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
+validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }
+end

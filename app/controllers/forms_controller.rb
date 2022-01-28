69fa3b399c02f66f27778d512ec4bb0e179060b8
hirona96 <95573824+hirona96@users.noreply.github.com>
2022-01-28 13:46:36 +0900

    topsコントローラー、ルーティングの設定

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,20 @@
+class FormsController < ApplicationController
+  def index
+    @form = Form.new
+  end
+
+  def create
+    @form = Form.new(form_params)
+    if @form.save
+      render :create
+    else
+      render :index
+    end
+  end
+
+  private 
+  
+  def form_params
+    params.require(:form).permit(:last_name_kana, :first_name_kana, :phone_number, :number_of_people, :datetime, :seat)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "tops#index"
 
   resources :tops
+  resources :forms, only: [:index, :create]
 end

23bdf1890d4013ffc56e5987bd4060023a8c55f3
hirona96 <95573824+hirona96@users.noreply.github.com>
2022-01-30 00:25:45 +0900

    ビューの編集、保存機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,13 @@ class CommentsController < ApplicationController
 
   def index
     @comments = Comment.all
+    @comment = Comment.new
   end
 
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      render :index
+      redirect_to action: :index
     else
       render :index
     end
@@ -16,7 +17,7 @@ class CommentsController < ApplicationController
 
   private
 
-  def(comment_params)
+  def comment_params
     params.require(:comment).permit(:evaluation_id, :comment, :image) 
   end
 

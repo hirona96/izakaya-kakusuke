ab1ca748f408507610b37fca4d11c753b1aec00b
hirona96 <95573824+hirona96@users.noreply.github.com>
2022-02-02 15:18:01 +0900

    バリデーション、コントローラーの編集

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,15 @@
 class Employee < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :employee_num, format: { with: /\A[a-z0-9]+\z/, message: 'is invalid. Include both letters and numbers.'}
+    validates :name
+    validates :one_thing
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers.'
+
 end

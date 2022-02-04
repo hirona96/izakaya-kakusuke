89752140544feb91a0a716e2a59b68c92f1b31d2
hirona96 <95573824+hirona96@users.noreply.github.com>
2022-02-04 13:00:51 +0900

    createアクションの編集

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -1,9 +1,13 @@
 class RetirementsController < ApplicationController
-    
+
   def new
   end
 
   def create
+    if current_employee.destroy
+      reset_session
+      redirect_to root_path, notice: "退会完了しました"
+    end
   end
 
 end

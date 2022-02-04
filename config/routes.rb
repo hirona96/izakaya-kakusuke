9c68eb05275ceeb0504613bae0bd124177628455
hirona96 <95573824+hirona96@users.noreply.github.com>
2022-02-04 12:01:04 +0900

    コントローラー、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :forms, only: [:index, :create]
   resources :comments, only: [:index, :new, :create]
   resources :employees, only: [:index]
+  resources :retirments, only: [:new, :create]
 end
